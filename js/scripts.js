82756373d6fad1e3cf41d11bc3104aa3db76ff3d
AramGevorgyan09 <gevorgyanaram0909@gmail.com>
2025-11-03 21:12:05 +0400

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+const scrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  })
+}
+
+const scrollToTopElement = document.querySelector('.scroll-to-top')
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY >= Math.floor(window.innerHeight)) {
+    scrollToTopElement.className = 'scroll-to-top pointer active'
+  } else {
+    scrollToTopElement.className = 'scroll-to-top pointer'
+  }
+})
+
+const body = document.querySelector('body')
+const darkModeImg = document.querySelector('.dark-mode')
+const lightModeImg = document.querySelector('.light-mode')
+
+const changeTheme = () => {
+  if (darkModeImg.className.includes('active')) {
+    body.className = 'dark'
+    darkModeImg.className = ''
+    lightModeImg.className = 'active'
+  } else {
+    body.className = ''
+    darkModeImg.className = 'active'
+    lightModeImg.className = ''
+  }
+}
